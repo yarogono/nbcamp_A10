5339c9144e2e3a2e0f5bfadc2985c5a63340cd7a
yarogono <70641418+yarogono@users.noreply.github.com>
2023-08-09 19:45:30 +0900

    feat/CardInitAnimation

--- Assets/Scripts/Managers/CardManager.cs
+++ Assets/Scripts/Managers/CardManager.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
     public GameObject card;
     float timer;
+    private bool isCardGenerated;
+
+    Dictionary<GameObject, Vector3> cardList = new Dictionary<GameObject, Vector3>();
 
     [HideInInspector] public GameObject firstCard;
     [HideInInspector] public GameObject secondCard;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isCardGenerated = false;
     }
 
     // Update is called once per frame
@@ -27,6 +32,11 @@ public class CardManager : MonoBehaviour
                 timer = 0;
             }
         }
+
+        if (isCardGenerated == false)
+        {
+            StartCoroutine(GenerateCardMoveToTarget(0.03f));
+        }
     }
 
     public void GenerateCard() {
@@ -39,11 +49,25 @@ public class CardManager : MonoBehaviour
             
             float x = (i%4) * 1.4f - 2.1f;
             float y = (i/4) * 1.4f - 3.0f;
-            newCard.transform.position = new Vector3(x, y, 0);
+            newCard.transform.position = new Vector3(0f, 0f, 0f);
 
+            Vector3 target = new Vector3(x, y, 0);
             string cardName = "card" + cards[i].ToString();
             newCard.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(cardName);
+            cardList.Add(newCard, target);
         }
+    }
+
+    IEnumerator GenerateCardMoveToTarget(float waitSeconds)
+    {
+        foreach (KeyValuePair<GameObject, Vector3> card in cardList)
+        {
+            GameObject cardGameObject = card.Key;
+            Vector3 cardVector3 = card.Value;
+            cardGameObject.transform.position = Vector3.Lerp(cardGameObject.transform.position, cardVector3, 0.1f);
+            yield return new WaitForSeconds(waitSeconds);
+        }
+        isCardGenerated = true;
     }
 
     public void IsMatched() {
