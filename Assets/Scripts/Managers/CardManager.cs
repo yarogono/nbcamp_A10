827fea448f7a2ce46ed1f59be5ee1ca3bee7aaf3
yarogono <70641418+yarogono@users.noreply.github.com>
2023-08-13 11:23:54 +0900

    refactor/CardManager 코드 수정

--- Assets/Scripts/Managers/CardManager.cs
+++ Assets/Scripts/Managers/CardManager.cs
@@ -6,38 +6,39 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    private float _timer;
+    private int _cardsLeft;
+    private bool _isCardGenerated;
+
+    private Dictionary<GameObject, Vector3> _cardList = new Dictionary<GameObject, Vector3>();
+
     public GameObject card;
-    float timer;
-    int cardsLeft;
 
-    private bool isCardGenerated;
+    [HideInInspector] 
+    public GameObject firstCard;
 
-    Dictionary<GameObject, Vector3> cardList = new Dictionary<GameObject, Vector3>();
+    [HideInInspector] 
+    public GameObject secondCard;
 
-    [HideInInspector] public GameObject firstCard;
-    [HideInInspector] public GameObject secondCard;
-    // Start is called before the first frame update
     void Start()
     {
-        isCardGenerated = false;
+        _isCardGenerated = false;
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         if(firstCard != null) {
-            timer += Time.deltaTime;
-            if(timer >= 5) {
+            _timer += Time.deltaTime;
+            if(_timer >= 5) {
                 firstCard.GetComponent<Card>().CloseCard();
                 firstCard = null;
-                timer = 0;
+                _timer = 0;
             }
         }
 
-        if (isCardGenerated == false)
+        if (_isCardGenerated == false)
         {
-            StartCoroutine(GenerateCardMoveToTarget(0.03f));
+            StartCoroutine(CardListMoveToDestination(0.03f));
         }
     }
 
@@ -45,35 +46,40 @@ public class CardManager : MonoBehaviour
         int[] cards = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
         cards = cards.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
-        cardsLeft = cards.Length;
+        _cardsLeft = cards.Length;
 
         float cardTerm = card.transform.localScale.x + 0.1f;
-            
+
+        GameObject cardsGameObject = GameObject.Find("Cards");
+
+        if (cardsGameObject == null)
+            return;
+        
         for(int i = 0; i < 16; i++) {
             GameObject newCard = Instantiate(card);
-            newCard.transform.parent = GameObject.Find("Cards").transform;
+            newCard.transform.parent = cardsGameObject.transform;
 
             float x = (i % 4) * 1.4f - 2.1f;
             float y = (i / 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(0f, 0f, 0f);
 
-            Vector3 target = new Vector3(x, y, 0);
-            string cardName = "card" + cards[i].ToString();
+            Vector3 destnation = new Vector3(x, y, 0);
+            string cardName = $"card{cards[i]}";
             newCard.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(cardName);
-            cardList.Add(newCard, target);
+            _cardList.Add(newCard, destnation);
         }
     }
 
-    IEnumerator GenerateCardMoveToTarget(float waitSeconds)
+    private IEnumerator CardListMoveToDestination(float waitSeconds)
     {
-        foreach (KeyValuePair<GameObject, Vector3> card in cardList)
+        foreach (KeyValuePair<GameObject, Vector3> card in _cardList)
         {
             GameObject cardGameObject = card.Key;
-            Vector3 cardVector3 = card.Value;
-            cardGameObject.transform.position = Vector3.Lerp(cardGameObject.transform.position, cardVector3, 0.1f);
+            Vector3 destination = card.Value;
+            cardGameObject.transform.position = Vector3.Lerp(cardGameObject.transform.position, destination, 0.1f);
             yield return new WaitForSeconds(waitSeconds);
         }
-        isCardGenerated = true;
+        _isCardGenerated = true;
     }
 
     public void IsMatched() {
@@ -86,9 +92,9 @@ public class CardManager : MonoBehaviour
             SoundManager.Instance.PlaySFX(SoundManager.SFX.matchSuccess);
             firstCard.GetComponent<Card>().DestroyCard();
             secondCard.GetComponent<Card>().DestroyCard();
-            cardsLeft -= 2;
+            _cardsLeft -= 2;
 
-            if(cardsLeft == 0) {
+            if(_cardsLeft == 0) {
                 GameManager.I.uiManager.ActiveEndText();
             }
         }
@@ -103,7 +109,7 @@ public class CardManager : MonoBehaviour
 
         firstCard = null;
         secondCard = null;
-        timer = 0;
+        _timer = 0;
     }
 
     string WhoAreYou(string name) {
